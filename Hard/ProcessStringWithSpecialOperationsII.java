5f027b211b8810b0064dd8d47c2a21569ad4861a
Sakshi kumari <sakshi.good787@gmail.com>
2026-06-17 19:22:28 +0530

    Create ProcessStringWithSpecialOperationsII.java

---
+++ Hard/ProcessStringWithSpecialOperationsII.java
@@ -0,0 +1,62 @@
+class Solution {
+
+    public char processStr(String s, long k) {
+
+        int n = s.length();
+        long[] len = new long[n + 1];
+
+        for (int i = 0; i < n; i++) {
+
+            char ch = s.charAt(i);
+
+            if (ch >= 'a' && ch <= 'z') {
+                len[i + 1] = len[i] + 1;
+            } else if (ch == '*') {
+                len[i + 1] = Math.max(0, len[i] - 1);
+            } else if (ch == '#') {
+                len[i + 1] = len[i] * 2;
+            } else {
+                len[i + 1] = len[i];
+            }
+        }
+
+        if (k >= len[n]) {
+            return '.';
+        }
+
+        for (int i = n - 1; i >= 0; i--) {
+
+            char ch = s.charAt(i);
+
+            if (ch >= 'a' && ch <= 'z') {
+
+                if (k == len[i]) {
+                    return ch;
+                }
+
+            } else if (ch == '#') {
+
+                k %= len[i];
+
+            } else if (ch == '%') {
+
+                if (len[i] > 0) {
+                    k = len[i] - 1 - k;
+                }
+
+            } else { // '*'
+
+                long before = len[i];
+
+                if (before > len[i + 1]) {
+
+                    if (k == before - 1) {
+                        return '.';
+                    }
+                }
+            }
+        }
+
+        return '.';
+    }
+}
